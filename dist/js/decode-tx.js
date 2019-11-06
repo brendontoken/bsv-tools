1149b231f94298b5fa75d612e79793600db8ccbc
Brendon Duncan <brendon.duncan@tokenized.com>
2019-11-06 17:08:06 +1300

    Feature: Indentation of decoded op_return.

--- dist/js/decode-tx.js
+++ dist/js/decode-tx.js
@@ -7,25 +7,35 @@ const b58ch = bsv.encoding.Base58Check;
 
 const sampleTx = "0200000002aa925d16dfaa731f8ce7b09517b443c486374b3691e1f41128d9f682c434ce87000000006a47304402201d98763aae4206f1211020fa3a3744e758425ea1c7ee80574ac10742b8f3e1c902201b340d1a33e65e4f1d14bf7ff42da070da734c80891947559ea7bc2fa67f741c41210372506b7c3e6965e959f1bba50f6df46601fc5e34f0e5ba8d50a711c9797d1ae5ffffffffb2684cf1c88febd3ba2661f71e052bcc9fa67ccc0c0cd8770ed6b0e8b898c8d80100000069463043022021ecbbc769329055377dc0df56bf1cebfdd31ef54d6a73dc759988198129d257021f4cce425635bd47ea19ffd7fa7f144ce3b1a65313f3b38b99c348cf5eb050ac4121039d675257b7ea4d3e97bab93d80050a2202dee3fb31cf9b3ca6a91d998bbb074bffffffff03e8030000000000001976a91469cd66052e7e85b4eb1d8c51efc3475bc9d5cc3c88ac000000000000000031006a02bd000e746573742e746f6b656e697a6564041a024d311712010018ec07220f0a0d547565736461792031313a353744080000000000001976a914519f1998edb2d5fa2d187bee59ac078e4a43e95088ac00000000";
 
-function addDecodedRow(txPortion, decoded, showTopSeparator, isAHeading, showIntermedieateTopSeparator) {
+function addDecodedRow(txPortion, decoded, options) {
+  options = options || {};
+  const showTopSeparator = options.showTopSeparator;
+  const isAHeading = options.isAHeading;
+  const showIntermediateTopSeparator = options.showIntermediateTopSeparator;
+  const indented = options.indented;
+
   const row = document.createElement("tr");
   const txCell = document.createElement("td");
   const decodedCell = document.createElement("td")
   const txDiv = document.createElement("div")
 
-
   txDiv.className = "monospace";
   txDiv.innerText = txPortion;
-  txDiv.style = "width: 50vw; overflow-wrap: break-word";
+  txDiv.style.width = "50vw"
+  txDiv.style.overflowWrap= "break-word";
   decodedCell.innerHTML = decoded;
   if (showTopSeparator) {
-    row.style = "border-top: 1px solid lightgrey"
+    row.style.borderTop = "1px solid lightgrey"
   }
-  if (showIntermedieateTopSeparator) {
-    row.style = "border-top: 1px dashed lightgrey"
+  if (showIntermediateTopSeparator) {
+    row.style.borderTop = "1px dashed lightgrey"
   }
   if (isAHeading) {
-    decodedCell.style = "font-weight: bold"
+    decodedCell.style.fontWeight = "bold"
+  }
+  if (indented) {
+    txDiv.style.paddingLeft = "2em"
+    decodedCell.style.paddingLeft= "2em"
   }
 
   txCell.appendChild(txDiv);
@@ -85,7 +95,7 @@ function decodeTx(tx) {
   let i;
   for (i = 0; i < vin; i++) {
     console.log(`----------------  Input ${i}`);
-    addDecodedRow("", `Input ${i}`, true, true);
+    addDecodedRow("", `Input ${i}`, { showTopSeparator: true, isAHeading: true});
     const txidRaw = tx.slice(cursor, cursor + 64);
     const txid = reverseHex(txidRaw);
     console.log(`${txidRaw}  txid: ${txid}`)
@@ -124,7 +134,7 @@ function decodeTx(tx) {
   
   for (i = 0; i < vout; i++) {
     console.log(`----------------  Output ${i}`);
-    addDecodedRow("", `Output ${i}`, true, true);
+    addDecodedRow("", `Output ${i}`, { showTopSeparator: true, isAHeading: true });
     const nValueRaw = tx.slice(cursor, cursor + 16);
     const nValueReversed = reverseHex(nValueRaw);
     const nValue = Number.parseInt(nValueReversed, 16);
@@ -147,7 +157,7 @@ function decodeTx(tx) {
     let or = "";
     let orCursor = 0;
     if (lockingScriptRaw.startsWith("006a")) {
-      addDecodedRow("006a", "&nbsp;&nbsp;OP_RETURN", false, true, true);
+      addDecodedRow("006a", "OP_RETURN", { isAHeading: true, showIntermediateTopSeparator: true, indented: true });
       or = or + lockingScriptRaw.slice(0, 4);
       orCursor = 4;
   
@@ -166,12 +176,12 @@ function decodeTx(tx) {
         or = or + ` ${pushSizeRaw}`;
         
         console.log(`Push size: 0x${pushSizeRaw} (${pushSize})`);
-        addDecodedRow(`${pushSizeRaw}`,`&nbsp;&nbsp;Push data: ${pushSize}`);
+        addDecodedRow(`${pushSizeRaw}`,`Push data: ${pushSize}`, { indented: true });
   
         const pushDataRaw = lockingScriptRaw.slice(orCursor, orCursor + 2 * pushSize);
         or = or + ` ${pushDataRaw}`
         const interpretedData = interpretData(pushSize, pushDataRaw, pushIndex);
-        addDecodedRow(`${pushDataRaw}`, `${interpretedData ? `&nbsp;&nbsp;${interpretedData}` : "&nbsp;&nbsp;Pushed data"}`)
+        addDecodedRow(`${pushDataRaw}`, `${interpretedData ? `${interpretedData}` : "Pushed data"}`, { indented: true });
         orCursor += pushSize * 2;
         pushIndex += 1;
       }
@@ -185,7 +195,7 @@ function decodeTx(tx) {
   const nLockTimeReversed = reverseHex(nLockTimeRaw);
   const nLockTime = Number.parseInt(nLockTimeReversed, 16);
   console.log(`${nLockTimeRaw}          nLockTime: ${nLockTime}`);
-  addDecodedRow(`${nLockTimeRaw}`, `nLockTime: ${nLockTime}`, true);
+  addDecodedRow(`${nLockTimeRaw}`, `nLockTime: ${nLockTime}`, { showTopSeparator: true });
 }
 
 function interpretData(byteSize, hex, index) {
