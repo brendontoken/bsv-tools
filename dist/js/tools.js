933d729662576cc565aaa1bc52f1546e3d69bd4c
Brendon Duncan <brendon.duncan@tokenized.com>
2019-11-14 16:54:37 +1300

    Feature: Accept a wider range of hashed addresses.

--- dist/js/tools.js
+++ dist/js/tools.js
@@ -1,4 +1,5 @@
 const addressFromPublicKeyHashElement = document.getElementById('address-from-pkhash');
+const addressInterpretedElement = document.getElementById('address-interpreted');
 const base58CheckFromHexInputElement = document.getElementById('base58check-hex');
 const base58CheckFromHexOutputElement = document.getElementById('base58check-from-hex');
 const publicKeyHashInputElement = document.getElementById('pkhash');
@@ -21,6 +22,18 @@ function reverseHex(hex) {
   return reversed;
 }
 
+function trimQuotes(s) {
+  if (s.startsWith('"')) {
+    s = s.slice(1);
+  }
+
+  if (s.endsWith('"')) {
+    s = s.slice(-1);
+  }
+
+  return s;
+}
+
 base58CheckFromHexInputElement.addEventListener("input", function onBase58CheckInput(event) {
   const content = event.target.value;
   try {
@@ -34,15 +47,32 @@ base58CheckFromHexInputElement.addEventListener("input", function onBase58CheckI
 
 publicKeyHashInputElement.addEventListener("input", function onPublicKeyHashInput(event) {
   const content = event.target.value;
-  if (content.length === 40) {
+  let trimmed = trimQuotes(content);
+
+  let interpreted = "";
+  // Remove leading type indicator
+  if (trimmed.length === 42 && trimmed.startsWith("20")) {
+    trimmed = trimmed.slice(2);
+    interpreted = `20 ${trimmed}`;
+  }
+
+  // Handle full P2PKH locking script, ie 76a914c002c97c1c86d9f243bb03c647bc6492dc12bc7688ac
+  if (trimmed.length === 50 && trimmed.startsWith("76a914") && trimmed.endsWith("88ac")) {
+    trimmed = trimmed.slice(6, -4);
+    interpreted = `76 a9 14 ${trimmed} 88 ac`;
+  }
+
+  if (trimmed.length === 40) {
     console.log("bsv:", bsv);
-    const prefixed = "00" + content;
+    const prefixed = "00" + trimmed;
     const b = bsv.encoding.Base58Check.fromHex(prefixed);
     const addr = b.toString();
     addressFromPublicKeyHashElement.innerHTML = `<a href="https://whatsonchain.com/address/${addr}" target="_blank">${addr}</a>`;
   } else {
-    addressFromPublicKeyHashElement.innerText = `Input is not 40 characters. (${content.length})`;
+    addressFromPublicKeyHashElement.innerText = `Input is not 40 characters (${content.length}), or full P2PKH locking script.`;
   }
+
+  addressInterpretedElement.innerText = interpreted;
 });
 
 reverseInputElement.addEventListener("input", function onReverseInput(event) {
